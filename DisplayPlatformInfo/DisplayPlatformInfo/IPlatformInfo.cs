17859d251365d17931f41b0c1ec96175f0f90406
Joshua Schipull <joshschipull@gmail.com>
2018-03-24 09:15:45 -0500

    Add project files.

---
+++ DisplayPlatformInfo/DisplayPlatformInfo/IPlatformInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DisplayPlatformInfo
+{
+    public interface IPlatformInfo
+    {
+        string GetModel();
+
+        string GetVersion();
+    }
+}
---
+++ DisplayPlatformInfo/DisplayPlatformInfo/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DisplayPlatformInfo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            IPlatformInfo platformInfo = DependencyService.Get<IPlatformInfo>();
+            modelLabel.Text = platformInfo.GetModel();
+            versionLabel.Text = platformInfo.GetVersion();
+        }
+	}
+}
